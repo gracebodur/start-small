90c8848d0c9deb7e2408be2a21b7919496cad779
Grace <mgracebodur@gmail.com>
2020-01-06 23:28:04 -0500

    Updated heading

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <div>
     <header className="App-header">
         <h1>START small</h1>
-        <h3>A charitable platform that aims to improve every child's education.</h3>
+        <h3>A charity app that searches multiple IRS registered nonprofits</h3>
     </header>
     </div>
     )
