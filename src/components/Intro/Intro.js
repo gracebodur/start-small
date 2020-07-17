0c1953747fa3be6a5a29f76b283e107e022d6718
Grace <mgracebodur@gmail.com>
2020-07-17 14:38:17 -0400

    Change intro font sizes

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -5,10 +5,10 @@ import './Intro.css'
 function Intro() {
     return (
         <div>
-            <div className='Intro'>
+            <div className='Proj-intro'>
                 <section>
-                    <h1 className='proj-name'>Start small</h1>
-                        <ul>
+                    <h1 className='Proj-name'>Start small</h1>
+                        <ul className='Proj-details'>
                             <li>Pick a classroom project for your friends to support.</li>
                             <li>Let us know of classroom projects that you think needs help</li>
                             <li>Make a big difference!</li>
@@ -17,7 +17,7 @@ function Intro() {
             </div>
             <div>
                 <ContactButton />
-                <p className='demo'>Demo: Username: admin Password: admin</p>
+                <p className='Proj-demo'>Demo: Username: admin Password: admin</p>
             </div>
         </div>
     )
