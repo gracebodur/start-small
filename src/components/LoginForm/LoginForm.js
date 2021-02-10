bbb8e3ff1288a367098abe41fe770c83f98f3a29
Grace <mgracebodur@gmail.com>
2021-02-10 09:17:27 -0500

    add bootstrap classname to register form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -5,92 +5,89 @@ import { Section } from '../../components/Utils/Utils'
 import './LoginForm.css'
 
 class LoginForm extends Component {
-    static defaultProps = {
-        onLoginSuccess: () => { }
-    }
+	static defaultProps = {
+		onLoginSuccess: () => {},
+	}
 
-    constructor ( props ) {
-        super( props )
-        this.state = {
-            user_name: '',
-            password: '',
-            error: null
-        }
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			user_name: '',
+			password: '',
+			error: null,
+		}
+	}
 
-    handleSubmitBasicAuth = ev => {
-        ev.preventDefault()
-        const { user_name, password } = ev.target
+	handleSubmitBasicAuth = (ev) => {
+		ev.preventDefault()
+		const { user_name, password } = ev.target
 
-        TokenService.saveAuthToken(
-            TokenService.makeBasicAuthToken( user_name.value, password.value )
-        )
-        user_name.value = ''
-        password.value = ''
-        this.props.onLoginSuccess()
-    }
+		TokenService.saveAuthToken(TokenService.makeBasicAuthToken(user_name.value, password.value))
+		user_name.value = ''
+		password.value = ''
+		this.props.onLoginSuccess()
+	}
 
-    handleSubmitJwtAuth = ev => {
-        ev.preventDefault()
-        this.setState( { error: null } )
-        const { user_name, password } = ev.target
+	handleSubmitJwtAuth = (ev) => {
+		ev.preventDefault()
+		this.setState({ error: null })
+		const { user_name, password } = ev.target
 
-        AuthApiService.postLogin( {
-            user_name: user_name.value,
-            password: password.value,
-        } )
-            .then( res => {
-                user_name.value = ''
-                password.value = ''
-                TokenService.saveAuthToken( res.authToken )
-                this.props.onLoginSuccess()
-            } )
-            .catch( res => {
-                this.setState( { error: res.error } )
-            } )
-    }
+		AuthApiService.postLogin({
+			user_name: user_name.value,
+			password: password.value,
+		})
+			.then((res) => {
+				user_name.value = ''
+				password.value = ''
+				TokenService.saveAuthToken(res.authToken)
+				this.props.onLoginSuccess()
+			})
+			.catch((res) => {
+				this.setState({ error: res.error })
+			})
+	}
 
-    render () {
-        const { error } = this.state
-        return (
-            <Section className='container'>
-                <div className="login-form center_div row">
-                    <form onSubmit={this.handleSubmitJwtAuth} className='col-md-12 form-wrapper'>
-                        <h2 className='login-header h2'>Login</h2>
-                        <div role='alert'>
-                            {error && <p className='red'>{error}</p>}
-                        </div>
-                        <div className="form-group">
-                            <label className='h6'>Username</label>
-                            <input
-                                type="text"
-                                className="form-control form-input"
-                                id="form-username"
-                                name="user_name"
-                                placeholder="Type your username"
-                                required />
-                        </div>
-                        <div className="form-group">
-                            <label className='h6'>Password</label>
-                            <input
-                                type="password"
-                                className="form-control form-input"
-                                id="form-password"
-                                name="password"
-                                placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
-                                required />
-                        </div>
-                        <div className="form-group clearfix">
-                            <button type="submit" className="btn float-left">Sign in</button>
-                        </div>
-                    </form>
-                </div>
-            </Section>
-        )
-    }
-
+	render() {
+		const { error } = this.state
+		return (
+			<Section className='container'>
+				<div className='login-form center_div row'>
+					<form onSubmit={this.handleSubmitJwtAuth} className='col-md-12 form-wrapper'>
+						<h2 className='login-header h2'>Login</h2>
+						<div role='alert'>{error && <p className='red'>{error}</p>}</div>
+						<div className='form-group'>
+							<label className='h6'>Username</label>
+							<input
+								id='form-username'
+								type='text'
+								className='form-control form-input'
+								name='user_name'
+								placeholder='Type your username'
+								required
+							/>
+						</div>
+						<div className='form-group'>
+							<label className='h6'>Password</label>
+							<input
+								type='password'
+								className='form-control form-input'
+								id='form-password'
+								name='password'
+								placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;'
+								required
+							/>
+						</div>
+						<div className='form-group clearfix'>
+							<button type='submit' className='btn float-left'>
+								Sign in
+							</button>
+						</div>
+					</form>
+				</div>
+			</Section>
+		)
+	}
 }
 
 export default LoginForm
-
-
--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,88 +1,88 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Section } from '../../components/Utils/Utils'
 import AuthApiService from '../../services/auth-api-service'
 import './RegistrationForm.css'
 
 class RegistrationForm extends Component {
-  static defaultProps = {
-      onRegistrationSuccess: () => {}
-  }
+	static defaultProps = {
+		onRegistrationSuccess: () => {},
+	}
 
-  state = { error: null }
+	state = { error: null }
 
-  handleSubmit = ev => {
-    ev.preventDefault()
-    const { full_name, user_name, password } = ev.target
+	handleSubmit = (ev) => {
+		ev.preventDefault()
+		const { full_name, user_name, password } = ev.target
 
-    this.setState({ error: null })
-    AuthApiService.postUser({
-      user_name: user_name.value,
-      password: password.value,
-      full_name: full_name.value,
-    })
-      .then(user => {
-        full_name.value = ''
-        user_name.value = ''
-        password.value = ''
-        this.props.onRegistrationSuccess()
-      })
-      .catch(res => {
-        this.setState({ error: res.error })
-      })
-  }
- 
-  render() {
-    const { error }  = this.state
-      return(
-        <Section className='container'>
-                <div className="reg-form center_div row">
-                    <form onSubmit={this.handleSubmit} className='col-md-12 form-wrapper'>
-                        <h2 className='reg-header h2'>Register</h2>
-                        <div role='alert'>
-                            {error && <p className='red'>{error}</p>}
-                        </div>
-                        <div className="form-group">
-                            <label className='h6'>Full Name</label>
-                            <input
-                                id = "reg-fullname"
-                                className = "reg-input"
-                                type = "text"
-                                name = "full_name"
-                                placeholder = "Type your full name"
-                                required />
-                        </div>
-                        <div className="form-group">
-                            <label className='h6'>User Name</label>
-                            <input
-                                id = "reg-username"
-                                className = "reg-input"
-                                type = "text"
-                                name = "user_name"
-                                placeholder = "Type your username"
-                                required />
-                        </div>
-                        <div className="form-group">
-                            <label className='h6'>Password</label>
-                            <input
-                                id = "reg-password"
-                                className = 'reg-input'
-                                type = "password"
-                                name = "password"
-                                placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
-                                required />
-                        </div>
-                        <div className="form-group clearfix">
-                            <button type="submit" className="btn float-left">Register</button>
-                        </div>
-                    </form>
-                </div>
-            </Section>
-      )
-    }
+		this.setState({ error: null })
+		AuthApiService.postUser({
+			user_name: user_name.value,
+			password: password.value,
+			full_name: full_name.value,
+		})
+			.then((user) => {
+				full_name.value = ''
+				user_name.value = ''
+				password.value = ''
+				this.props.onRegistrationSuccess()
+			})
+			.catch((res) => {
+				this.setState({ error: res.error })
+			})
+	}
+
+	render() {
+		const { error } = this.state
+		return (
+			<Section className='container'>
+				<div className='reg-form center_div row'>
+					<form onSubmit={this.handleSubmit} className='col-md-12 form-wrapper'>
+						<h2 className='reg-header h2'>Register</h2>
+						<div role='alert'>{error && <p className='red'>{error}</p>}</div>
+						<div className='form-group'>
+							<label className='h6'>Full Name</label>
+							<input
+								id='reg-fullname'
+								className='form-control reg-input'
+								type='text'
+								name='full_name'
+								placeholder='Type your full name'
+								required
+							/>
+						</div>
+						<div className='form-group'>
+							<label className='h6'>User Name</label>
+							<input
+								id='reg-username'
+								className='form-control reg-input'
+								type='text'
+								name='user_name'
+								placeholder='Type your username'
+								required
+							/>
+						</div>
+						<div className='form-group'>
+							<label className='h6'>Password</label>
+							<input
+								id='reg-password'
+								className='form-control reg-input'
+								type='password'
+								name='password'
+								placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;'
+								required
+							/>
+						</div>
+						<div className='form-group clearfix'>
+							<button type='submit' className='btn float-left'>
+								Register
+							</button>
+						</div>
+					</form>
+				</div>
+			</Section>
+		)
+	}
 }
-       
 
 export default RegistrationForm
-
-
